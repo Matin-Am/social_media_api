f73c65f14b9ec9dc097939f083566c5a7c020569
Matin.Am <matin.amani101013@gmail.com>
2025-07-06 18:07:05 +0330

    starting implementation of custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,32 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser , AbstractBaseUser
 # Create your models here.
+
+
+
+
+class User(AbstractBaseUser):
+    phone_number = models.CharField(max_length=11 , unique=True , verbose_name="Phone number")
+    email = models.EmailField(max_length=100,unique=True,verbose_name="Email adress")
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+
+    REQUIRED_FIELDS = ['phone_number',"email"]
+    USERNAME_FIELD = "phone_number"
+
+
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self,perm,obj=None):
+        return True
+    
+    def has_module_perms(self,app_label):
+        return True
+    
+    @property
+    def is_staff(self):
+        return self.is_admin
+    
