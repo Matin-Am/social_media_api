ac00747265826e2a587fca5e32d99eaf0dc87d7a
Matin.Am <matin.amani101013@gmail.com>
2025-07-07 16:14:13 +0330

    Start registration with otp code and made its serialzer

--- accounts/models.py
+++ accounts/models.py
@@ -32,3 +32,10 @@ class User(AbstractBaseUser):
     def is_staff(self):
         return self.is_admin
     
+
+
+
+class OtpCode(models.Model):
+    code = models.SmallIntegerField()
+    email = models.EmailField(max_length=100)
+    created = models.DateTimeField(auto_now=True)
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from rest_framework.views import View
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserRegistrationSerializer
+from .models import User
 # Create your views here.
+
+class UserRegistrationAPI(View):
+    serializer_class = UserRegistrationSerializer
+    def post(self,request):
+        ser_data = self.serializer_class(data=request.data)
+        if ser_data.is_valid():
+            cd = ser_data.validated_data
+           
