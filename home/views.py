e8b1a011d17be41bb3f177fb22f572af595a0749
Matin.Am <matin.amani101013@gmail.com>
2025-08-17 23:14:20 +0330

    performed CRUD operations with ViewSets and routers

--- home/views.py
+++ home/views.py
@@ -1,56 +1,67 @@
 from django.shortcuts import render , get_object_or_404 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status , viewsets
 from rest_framework.authentication import TokenAuthentication
 from .serializers import PostSerializer
 from .custome_permissions import AdminOrIsowneronlyPermission , FollowOthersPermission
 from rest_framework.permissions import IsAuthenticated
 from .models import Post , Relation
 from accounts.models import User
-from rest_framework.viewsets import ViewSet
+
 # Create your views here.
 
-class UserListPostsAPI(APIView):
+class PostViewSet(viewsets.ViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = []
     serializer_class = PostSerializer
-    def get(self,request):
-       posts = Post.objects.all()
-       ser_data = self.serializer_class(instance=posts,many=True)
-       return Response(ser_data.data)
 
 
-class UserCreatePostAPI(APIView):
-    authentication_classes = [TokenAuthentication,]
-    serializer_class = PostSerializer
-    def post(self,request):
+    def get_queryset(self):
+        return Post.objects.all()
+    
+
+    def list(self,request):
+        ser_data = self.serializer_class(self.get_queryset(),many=True)
+        return Response(ser_data.data,status=status.HTTP_200_OK)
+    
+    def retrieve(self,request,pk=None):
+        post = get_object_or_404(self.get_queryset(),pk=pk)
+        ser_data = self.serializer_class(post).data
+        return Response(ser_data,status=status.HTTP_200_OK)
+
+
+    def partial_update(self,request,pk=None):
+        post = get_object_or_404(self.get_queryset(),id=pk)
+        self.check_object_permissions(request,post)
+        ser_data = self.serializer_class(data=request.data,instance=post,partial=True)
+        if ser_data.is_valid():
+            new_post = ser_data.save()
+            return Response(self.serializer_class(new_post).data,status=status.HTTP_200_OK)
+        return Response(ser_data.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+
+    def create(self,request):
         ser_data = self.serializer_class(data=request.data,context={"request":request})
         if ser_data.is_valid():
-            post = ser_data.create(ser_data.validated_data)
-            return Response(data=self.serializer_class(post).data,status=status.HTTP_201_CREATED)
-        return Response(ser_data.errors,status=status.HTTP_400_BAD_REQUEST)
-
-class UserUpdatePostAPI(APIView):
-    authentication_classes = [TokenAuthentication,]
-    permission_classes = [AdminOrIsowneronlyPermission]
-    serializer_class = PostSerializer
-    def put(self,request,post_id):
-        post = Post.objects.get(pk=post_id)
-        self.check_object_permissions(request,post)
-        ser_data =  self.serializer_class(data=request.data, instance=post,partial=True)
-        if ser_data.is_valid():
-            post = ser_data.save()  
+            post = ser_data.save()
             return Response(self.serializer_class(post).data,status=status.HTTP_200_OK)
         return Response(ser_data.errors,status=status.HTTP_400_BAD_REQUEST)
-    
-class UserDeletePostAPI(APIView):
-    authentication_classes = [TokenAuthentication,]
-    permission_classes = [AdminOrIsowneronlyPermission,]
-    def delete(self,request,post_id):
-        post = get_object_or_404(Post , id = post_id)
-        self.check_object_permissions(request , post)
+        
+    def destroy(self,request,pk=None):
+        post = get_object_or_404(self.get_queryset(),pk=pk)
+        self.check_object_permissions(request,post)
         post.delete()
-        return Response({"message":"post has been deleted successfully"},status=status.HTTP_200_OK)
+        return Response({"Message":"Post deleted sucessfully"},status=status.HTTP_200_OK)
+
+    def get_permissions(self):
+        if self.action == "destroy" or self.action == "partial_update":
+            self.permission_classes = [AdminOrIsowneronlyPermission]
+        if self.action == "create":
+            self.permission_classes = [IsAuthenticated]
+        return [permission() for permission in self.permission_classes]
     
+
 class UserFollowAPI(APIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [FollowOthersPermission]
