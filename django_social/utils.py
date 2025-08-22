f4a1ac2f25c7acfdf27bb4fff6c4cf06a990a5f8
Matin.Am <matin.amani101013@gmail.com>
2025-08-22 16:54:32 +0330

    changed utils file

--- django_social/utils.py
+++ django_social/utils.py
@@ -3,7 +3,7 @@ from django.core.mail import send_mail
 def send_otp_code(email,code):
     send_mail(
         subject="Otp Code" ,
-        message= f"Your otp code is : {code}",
+        message= f"Your otp code is  : {code}",
         from_email="matin.amani101013@email.com" , 
         recipient_list = [email,]
     )
